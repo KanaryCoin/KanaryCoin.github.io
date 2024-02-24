fc4e100c05a8d3c5e845835b841b6404b112aa24
KanaryCoin <kanary.polkadot@gmail.com>
2024-02-24 18:59:36 +0100

    Deploy website - based on f0c0f1099535fb598f2dfea30a9032e9f0a09348

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/carlos/KanaryCoin.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/carlos/KanaryCoin.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/carlos/KanaryCoin.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/carlos/KanaryCoin.github.io/src/css/custom.css"),
-];
